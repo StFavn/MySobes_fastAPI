63e93d5a7f8cd1f8f9dfbfa429076d1dd7f63848
Stefan <jadziaro@student.21-school.ru>
2024-05-03 03:31:35 +0700

    (client) Немного красоты для TopicTree

--- client/src/components/TopicSection.jsx
+++ client/src/components/TopicSection.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import './styles/tree.css'
+import '../styles/TopicTree.css'
 
 export default function TopicsSection() {
     const [loading, setLoading] = useState(false) // стейт для загрузки данных
@@ -22,63 +22,35 @@ export default function TopicsSection() {
 
   function TopicTree({ topic }) {
     return (
-      <li>
-        <details>
-          <summary>{topic.name}</summary>
-          <ul>
-            {topic.children.map((child) => (
+      // <details open={isOpen} onClick={toggleAccordion}>
+      <details>
+        <summary>{topic.name}</summary>
+        <ul>
+          {topic.children.map((child) => (
               <TopicTree key={child.id} topic={child} />
-            ))}
+          ))}
 
-            {topic.questions.map((question) => 
-              <li>{question.question}</li>
-            )}
-          </ul>
-        </details>
-      </li>
+          {topic.questions.map((question) => 
+            <li className="tree-question">{question.question}</li>
+          )}
+        </ul>
+      </details>
     )
   }
 
   return (
-    <section>
+    <section className="tree">
       { loading && <p>Загрузка...</p> }
       { !loading && 
-        <ul className="tree">
-          { topics.map((topic) =>
-            <TopicTree key={topic.id} topic={topic} />
-          )}
+        <ul>
+          <li>
+            { topics.map((topic) =>
+                <TopicTree key={topic.id} topic={topic} />
+            )}
+          </li>
         </ul> 
       }
     </section>
   )
 }
-
-
-  // function formatTopics(topics, level = 0) {
-  //   let formattedTopics = [];
-  //   for (const topic of topics) {
-  //     formattedTopics.push({
-  //       // id: topic.id,
-  //       // parent_id: topic.parent_id,
-  //       // name: topic.name,
-  //       ...topic,
-  //       level: level
-  //     });
-  //     if (topic.children && topic.children.length > 0) {
-  //         formattedTopics = formattedTopics.concat(formatTopics(topic.children, level + 1));
-  //     }
-  //   }
-  //   return formattedTopics;
-  // }
-
-  // function renderTopics(topics) {
-  //   return (
-  //     <ul>
-  //       {topics.map((topic) => (
-  //         <li key={topic.name} style={{ paddingLeft: `${topic.level * 20}px` }}>
-  //           {topic.name}
-  //         </li>
-  //       ))}
-  //     </ul>
-  //   );
-  // }
+// <li> я добавил только для того, чтобы был фон. Возможно это тупо
